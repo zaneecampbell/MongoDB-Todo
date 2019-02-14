4b1984f44190e98862061d9b17d10613dbc3419b
Zane Campbell <zanegitinit@gmail.com>
2019-02-13 23:45:08 -0800

    used ES6 destructuring to pull from mongodb

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     if (err) {
@@ -18,17 +19,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // });
 
-    db.collection('Users').insertOne({
-        name: 'Zane',
-        age: 27,
-        location: 'Washington'
-    }, (err, result) => {
-        if (err) {
-            return console.log('Unable to insert user', err);
-        }
+    // db.collection('Users').insertOne({
+    //     name: 'Zane',
+    //     age: 27,
+    //     location: 'Washington'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert user', err);
+    //     }
 
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp()));
+    // });
 
     client.close();
 });
